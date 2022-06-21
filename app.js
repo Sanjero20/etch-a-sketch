4acd2a0873371105a778656009245e3161bbefb9
Sanjero20 <gelosantos72@gmail.com>
2022-06-21 15:06:25 +0800

    Add clear button

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
-let grid = 0;
+let grid = 16;
+let boxSize = 100 /grid;
+// do {
+//   grid= window.prompt("Create a grid of: (2-64)");
+// }while (grid < 2 || grid > 64)
+const container = document.querySelector('.container');
+const clearBtn = document.querySelector('#clear');
 
-do {
-  grid= window.prompt("Create a grid of: (2-100)");
-}while (grid < 2 || grid > 100)
+// Create a Grid
+createGrid(grid);
 
-let gridSize = 100 /grid;
-console.log("size per box:", gridSize);
+const boxs = document.querySelectorAll('.container div');
+boxs.forEach(box => {
+  box.addEventListener('mouseover', () => {
+    addColor(box);
+  });
+});
 
-const container = document.querySelector('.container')
+clearBtn.onclick = () => {
+  boxs.forEach(box => {
+    box.classList.remove('change-color');
+  });
+}
 
-for (let i=0; i < grid * grid; i++) {
-  const div = document.createElement('div');
-  setBoxStyling(div);
-  container.appendChild(div);
+// Functions bruh
+function createGrid(grid){
+  for (let i=0; i < grid * grid; i++) {
+    const div = document.createElement('div');
+    setBoxStyling(div);
+    container.appendChild(div);
+  }
 }
 
 function setBoxStyling(element) {
-  element.setAttribute('style', 'border: 1px solid black');
-  element.style['width'] = `${gridSize}%`;
-  element.style['height'] = `${gridSize}%`;
+  // element.setAttribute('style', 'border: 1px solid black');
+  element.style['width'] = `${boxSize}%`;
+  element.style['height'] = `${boxSize}%`;
   element.style['box-sizing'] = 'border-box'; 
+}
+
+function addColor(element) {
+  element.classList.add('change-color');
+}
+
+function clearColor(element) {
+  console.log(element)
 }
