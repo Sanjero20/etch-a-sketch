8482fc4bb7f82cb652a199a0d47d91c9c0eb6ddb
Sanjero20 <gelosantos72@gmail.com>
2022-06-22 14:54:12 +0800

    Add slider to adjust grid

--- app.js
+++ app.js
@@ -1,47 +1,93 @@
-let grid = 16;
-let boxSize = 100 /grid;
-// do {
-//   grid= window.prompt("Create a grid of: (2-64)");
-// }while (grid < 2 || grid > 64)
+const gridLayout = document.querySelector('.grid-layout')
 const container = document.querySelector('.container');
 const clearBtn = document.querySelector('#clear');
+const slider = document.querySelector('.slider')
+let mousedown = false;
+
+let grid = slider.value;
+let boxSize;
+let boxs;
 
 // Create a Grid
-createGrid(grid);
+getGridSize(grid);
+checkMouseEvent();
+drawOnBox()
+clear()
 
-const boxs = document.querySelectorAll('.container div');
-boxs.forEach(box => {
-  box.addEventListener('mouseover', () => {
-    addColor(box);
-  });
-});
-
-clearBtn.onclick = () => {
-  boxs.forEach(box => {
-    box.classList.remove('change-color');
-  });
-}
 
 // Functions bruh
 function createGrid(grid){
+  boxSize = 100 /grid;
   for (let i=0; i < grid * grid; i++) {
     const div = document.createElement('div');
     setBoxStyling(div);
     container.appendChild(div);
   }
+  boxs = document.querySelectorAll('.container div');
 }
 
 function setBoxStyling(element) {
   // element.setAttribute('style', 'border: 1px solid black');
   element.style['width'] = `${boxSize}%`;
   element.style['height'] = `${boxSize}%`;
-  element.style['box-sizing'] = 'border-box'; 
+  element.style['border'] = '1px solid lightgrey';
+  element.style['box-sizing'] = 'border-box';
+  element.setAttribute('draggable', false);
 }
 
 function addColor(element) {
   element.classList.add('change-color');
 }
 
-function clearColor(element) {
-  console.log(element)
+function checkMouseEvent() {
+  // Check for mouse up and mouse down events
+
+  document.body.onmousedown = () => {
+    mousedown = true;
+    drawOnBox();
+  }
+  
+  document.body.onmouseup = () => {
+    mousedown = false;
+  }
+
+  document.body.addEventListener('drag', () => mousedown = false)
+}
+
+function getGridSize(grid) {
+  displayLayout(grid);
+  createGrid(grid);
+
+  slider.oninput = function() {
+    removeGrid();
+    displayLayout(slider.value)
+    createGrid(slider.value)
+    drawOnBox()
+  } 
+}
+
+function removeGrid() {
+  container.innerHTML = "";
+}
+
+function drawOnBox() {
+  boxs.forEach(box => {
+    box.addEventListener('mouseover', () => {
+      if (mousedown) {addColor(box)}
+    });
+
+    box.addEventListener('mousedown', () => addColor(box))
+  });
+}
+
+function clear() {
+  clearBtn.onclick = () => {
+    boxs.forEach(box => {
+      box.classList.remove('change-color');
+    });
+  }
+}
+
+function displayLayout(grid) {
+  gridLayout.innerHTML = `${grid}x${grid} Layout`
 }
